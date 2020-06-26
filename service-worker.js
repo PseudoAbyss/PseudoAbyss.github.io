50f57df30fec887ea18cf17cf83c20adec5441bd
Gustav Beije <gustavbeije@gmail.com>
2020-06-26 17:03:41 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pseudoabyss/pseudoabyss.github.io/precache-manifest.ebfa6d06902167937132b1c6c3201cbc.js"
+  "/precache-manifest.1ac2bf4c2ef540a76c9e18bab1ee26e8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pseudoabyss/pseudoabyss.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
