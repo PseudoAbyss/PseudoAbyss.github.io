f469e07fa05fea10c7f0161a7db6db5966be6050
Gustav Beije <gustavbeije@gmail.com>
2021-02-26 10:00:04 +0100

    Add CNAME

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: 'My Site',
   tagline: 'The tagline of my site',
   url: 'https://pseudoabyss.github.io',
-  baseUrl: '/',
+  baseUrl: '/build/index',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
