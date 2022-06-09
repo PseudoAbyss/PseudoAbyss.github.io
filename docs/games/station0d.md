39fff952cec971e17bb47e94e13fa3428103e1d4
phelioz <phelioz@users.noreply.github.com>
2022-06-09 14:03:38 +0000

    deploy: 4a52c7d43027f361df5d3ba17f05215463ea8d9b

--- docs/games/station0d.md
+++
@@ -1,9 +0,0 @@
----
-id: station0d
-title: Station 0D
-sidebar_label: Station 0D
----
-
-### A Sci-fi top-down immersive sim
-
-More info soon!
--- sidebars.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  gameSidebar: {
-    Games: ['games/station0d'],
-  },
-  musicSidebar: {
-    Music: ['music/thepurebox', 'music/0d'],
-  },
-};
